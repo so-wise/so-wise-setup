edf2be96b2246ede76704b5d80943118fddb471f
DanJonesOcean <dcjones.work@gmail.com>
2021-03-19 12:46:31 +0000

    Initial commit

---
+++ initial_setup.sh
@@ -0,0 +1,9 @@
+# initial setup 
+
+# set PYTHONPATH environment variable
+setenv PYTHONPATH /data/expose/MITgcm/utils/python/MITgcmutils
+
+# swap modules (might have to run this on the command line; doesn't work as script)
+module swap python/conda3 python/conda-python-2.7.14
+module load netcdf
+
